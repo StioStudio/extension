fea583153611d3614624741b9397061a77410ffd
StioStudio <simon.s.johanssen@gmail.com>
2023-01-04 16:01:16 +0100

    save

--- image.js
+++ image.js
@@ -15,45 +15,88 @@
  */
 
 let image = {
-    image: [],
-    name: []
+  image: [],
+  name: []
 };
 
-function getCanvas(){
-    let rem = document.querySelectorAll("canvas")
-    if(rem.length == 2){
-        return rem[0];
-    }
-    return rem[1];
+Scratch.renderer._gl
+
+function drawImage(tex, texWidth, texHeight, dstX, dstY, stampRotation) {
+  gl.bindTexture(gl.TEXTURE_2D, tex);
+
+  // Tell WebGL to use our shader program pair
+  gl.useProgram(program);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, quadColorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(quadColors), gl.STATIC_DRAW);
+  // Setup the attributes to pull data from our buffers
+  gl.bindBuffer(gl.ARRAY_BUFFER, quadPositionBuffer);
+  gl.enableVertexAttribArray(positionLocation);
+  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, quadTexCoordBuffer);
+  gl.enableVertexAttribArray(texcoordLocation);
+  gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, quadColorBuffer);
+  gl.enableVertexAttribArray(colorLocation);
+  gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
+
+  // this matrix will convert from pixels to clip space
+  var matrix = m4.orthographic(0, gl.canvas.width, gl.canvas.height, 0, -1, 1);
+
+  // this matrix will translate our quad to dstX, dstY
+  matrix = m4.translate(matrix, dstX, dstY, 0);
+
+  matrix = m4.zRotate(matrix, degreesToRadians(stampRotation))
+
+  // this matrix will scale our 1 unit quad
+  // from 1 unit to texWidth, texHeight units
+  matrix = m4.scale(matrix, texWidth, texHeight, 1);
+
+  // Set the matrix.
+  gl.uniformMatrix4fv(matrixLocation, false, matrix);
+
+  // Tell the shader to get the texture from texture unit 0
+  gl.uniform1i(textureLocation, 0);
+
+  // draw the quad (2 triangles, 6 vertices)
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
-(function(Scratch) {
+function getCanvas() {
+  let rem = document.querySelectorAll("canvas")
+  if (rem.length == 2) {
+    return rem[0];
+  }
+  return rem[1];
+}
+
+(function (Scratch) {
   'use strict';
   class MyExtension {
-    getInfo () {
-      return { 
+    getInfo() {
+      return {
         // `id` is the internal ID of the extension
         // It should never change!
         // If you choose to make an actual extension, please change this to something else.
         // Only the characters a-z and 0-9 can be used. No spaces or special characters.
         id: 'stio000image',
-  
+
         // `name` is what the user sees in the toolbox
         // It can be changed without breaking projects.
         name: 'image',
-  
+
         color1: '#119911',
-  
+
         blocks: [
           {
             opcode: 'load_image',
             blockType: Scratch.BlockType.REPORTER,
             text: 'load image[ONE]and wait',
             arguments: {
-                ONE: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'http://example.png'
-                }
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'http://example.png'
+              }
             }
           },
           {
@@ -61,14 +104,14 @@ function getCanvas(){
             blockType: Scratch.BlockType.COMMAND,
             text: 'save image[ONE][TWO]',
             arguments: {
-                ONE: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'name'
-                },
-                TWO: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'image'
-                }
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'name'
+              },
+              TWO: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'image'
+              }
             }
           },
           {
@@ -76,10 +119,10 @@ function getCanvas(){
             blockType: Scratch.BlockType.REPORTER,
             text: 'get image[ONE]',
             arguments: {
-                ONE: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'name'
-                }
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'name'
+              }
             }
           },
           {
@@ -87,39 +130,76 @@ function getCanvas(){
             blockType: Scratch.BlockType.COMMAND,
             text: 'stamp image[ONE] # NOT WORKING! #',
             arguments: {
-                ONE: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: 'image'
-                }
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'image'
+              }
             }
           },
         ]
       };
     }
-    load_image(args){
-        let img;
-        new Promise((resolve) => {
-            img = new Image();
-            img.onload = () => resolve(img);
-            img.src = args.ONE;
-        });
-        return img;
+    load_image(args) {
+      let img;
+      new Promise((resolve) => {
+        img = new Image();
+        img.onload = () => resolve(img);
+        img.src = args.ONE;
+      });
+      return img;
     }
-    save_image(args){
-        if(image.name.includes(args.ONE)){
-            image.image[image.name.indexOf(args.ONE)] = args.TWO
-            return;
-        }
-        image.name.push(args.ONE)
-        image.image.push(args.TWO)
+    save_image(args) {
+      if (image.name.includes(args.ONE)) {
+        image.image[image.name.indexOf(args.ONE)] = args.TWO
         return;
+      }
+      image.name.push(args.ONE)
+      image.image.push(args.TWO)
+      return;
     }
-    get_image(args){
-        return image.image[image.name.indexOf(args.ONE)]
+    get_image(args) {
+      return image.image[image.name.indexOf(args.ONE)]
     }
-    stamp_image(args){
-        
-        return;
+    stamp_image(args) {
+      gl.bindTexture(gl.TEXTURE_2D, tex);
+
+      // Tell WebGL to use our shader program pair
+      gl.useProgram(program);
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, quadColorBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(quadColors), gl.STATIC_DRAW);
+      // Setup the attributes to pull data from our buffers
+      gl.bindBuffer(gl.ARRAY_BUFFER, quadPositionBuffer);
+      gl.enableVertexAttribArray(positionLocation);
+      gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+      gl.bindBuffer(gl.ARRAY_BUFFER, quadTexCoordBuffer);
+      gl.enableVertexAttribArray(texcoordLocation);
+      gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
+      gl.bindBuffer(gl.ARRAY_BUFFER, quadColorBuffer);
+      gl.enableVertexAttribArray(colorLocation);
+      gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
+
+      // this matrix will convert from pixels to clip space
+      var matrix = m4.orthographic(0, gl.canvas.width, gl.canvas.height, 0, -1, 1);
+
+      // this matrix will translate our quad to dstX, dstY
+      matrix = m4.translate(matrix, dstX, dstY, 0);
+
+      matrix = m4.zRotate(matrix, degreesToRadians(stampRotation))
+
+      // this matrix will scale our 1 unit quad
+      // from 1 unit to texWidth, texHeight units
+      matrix = m4.scale(matrix, texWidth, texHeight, 1);
+
+      // Set the matrix.
+      gl.uniformMatrix4fv(matrixLocation, false, matrix);
+
+      // Tell the shader to get the texture from texture unit 0
+      gl.uniform1i(textureLocation, 0);
+
+      // draw the quad (2 triangles, 6 vertices)
+      gl.drawArrays(gl.TRIANGLES, 0, 6);
+      return;
     }
   }
   Scratch.extensions.register(new MyExtension());
