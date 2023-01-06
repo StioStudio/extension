f5aaf584b9c926e3bbc2c26589ff32ce09e02b37
StioStudio <simon.s.johanssen@gmail.com>
2023-01-06 08:58:17 +0100

    save

--- LogicGates.js
+++ LogicGates.js
@@ -1,84 +1,13 @@
-(function(Scratch) {
+(function (Scratch) {
   'use strict';
   class LogicGates {
-    getInfo () {
-      return { 
+    getInfo() {
+      return {
         id: 'LogicGates',
         name: 'Logic Gates',
         color1: '#59c059',
         color2: '#59c059',
         blocks: [
-          {
-            opcode: 'and',
-            blockType: Scratch.BlockType.BOOLEAN,
-            text: '[ONE] AND [TWO]',
-            arguments: {
-                ONE: {
-                    type: Scratch.ArgumentType.BOOLEAN,
-                    defaultValue: false
-                },
-                TWO: {
-                    type: Scratch.ArgumentType.BOOLEAN,
-                    defaultValue: false
-                }
-            }
-          },
-          {
-            opcode: 'nor',
-            blockType: Scratch.BlockType.BOOLEAN,
-            text: '[ONE] NOR [TWO]',
-            arguments: {
-                ONE: {
-                    type: Scratch.ArgumentType.BOOLEAN,
-                    defaultValue: false
-                },
-                TWO: {
-                    type: Scratch.ArgumentType.BOOLEAN,
-                    defaultValue: false
-                }
-            }
-          },
-          {
-            opcode: 'or',
-            blockType: Scratch.BlockType.BOOLEAN,
-            text: '[ONE] OR [TWO]',
-            arguments: {
-                ONE: {
-                    type: Scratch.ArgumentType.BOOLEAN,
-                    defaultValue: false
-                },
-                TWO: {
-                    type: Scratch.ArgumentType.BOOLEAN,
-                    defaultValue: false
-                }
-            }
-          },
-          {
-            opcode: 'xor',
-            blockType: Scratch.BlockType.BOOLEAN,
-            text: '[ONE] XOR [TWO]',
-            arguments: {
-                ONE: {
-                  type: Scratch.ArgumentType.BOOLEAN,
-                    defaultValue: false
-                  },
-                  TWO: {
-                    type: Scratch.ArgumentType.BOOLEAN,
-                    defaultValue: false
-                  }
-                }
-              },
-              {
-                opcode: 'not',
-                blockType: Scratch.BlockType.BOOLEAN,
-                text: 'NOT [ONE]',
-                arguments: {
-                    ONE: {
-                        type: Scratch.ArgumentType.BOOLEAN,
-                        defaultValue: false
-                    }
-                }
-              },
           {
             opcode: 'true',
             blockType: Scratch.BlockType.BOOLEAN,
@@ -88,16 +17,123 @@
             opcode: 'false',
             blockType: Scratch.BlockType.BOOLEAN,
             text: 'FALSE',
+          },
+          {
+            opcode: 'and',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: '[ONE] AND [TWO]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              },
+              TWO: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              }
+            }
+          },
+          {
+            opcode: 'nor',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: '[ONE] NOR [TWO]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              },
+              TWO: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              }
+            }
+          },
+          {
+            opcode: 'or',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: '[ONE] OR [TWO]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              },
+              TWO: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              }
+            }
+          },
+          {
+            opcode: 'xor',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: '[ONE] XOR [TWO]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              },
+              TWO: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              }
+            }
+          },
+          {
+            opcode: 'not',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'NOT [ONE]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.BOOLEAN,
+                defaultValue: false
+              }
+            }
+          },
+          {
+            opcode: 'equal2',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: '[ONE] == [TWO]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "Hi!"
+              },
+              TWO: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "Hi!"
+              }
+            }
+          },
+          {
+            opcode: 'equal3',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: '[ONE] === [TWO]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "Hi!"
+              },
+              TWO: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "Hi!"
+              }
+            }
           }
         ]
       };
+    }
+    true() {
+      return true;
+    }
+    false() {
+      return false;
     }
     and(args) {
       return args.ONE && args.TWO;
     }
     nor(args) {
       return !(args.ONE || args.TWO);
-    }    
+    }
     or(args) {
       return args.ONE || args.TWO;
     }
@@ -107,12 +143,12 @@
     not(args) {
       return !args.ONE;
     }
-    true() {
-      return true;
+    equal2(args) {
+      return args.ONE == args.TWO;
     }
-    false() {
-      return false;
+    equal3(args) {
+      return args.ONE === args.TWO;
     }
   }
-Scratch.extensions.register(new LogicGates());
+  Scratch.extensions.register(new LogicGates());
 })(Scratch);
